6de86a040ecc003bce206985c3df3b53c103c070
Oleg686 <71009817+Oleg686@users.noreply.github.com>
2022-10-30 20:14:44 +0300

    Calculator

---
+++ Coursework/Kyrsach_Diplom/Kyrsach_Diplom/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Org.Apache.Http.Cookies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kyrsach_Diplom
+{
+    public partial class MainPage : ContentPage
+    {
+        Entry cre = new Entry();
+        Entry ras = new Entry();
+    public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            StackLayout layout = new StackLayout();
+            Label label1 = new Label();
+            label1.Text = "Калькулятор автомобилиста";
+            label1.TextColor = Color.Red;
+            label1.FontSize = 20;
+            label1.FontAttributes = FontAttributes.Bold;
+            ras.Placeholder = "Введите расстояние";
+            cre.Placeholder = "Средний расход топлива на 100 км";
+            btn.Clicked += Btn_Clicked;
+            layout.Children.Add(label1);
+            layout.Children.Add(ras);
+            layout.Children.Add(cre);
+            layout.Children.Add(btn);
+            Content = layout;
+        }
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            btn.Text = Convert.ToString(Convert.ToDouble(ras.Text) * Convert.ToDouble(cre.Text) / 100);
+        }
+    }
+}
